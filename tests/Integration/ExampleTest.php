dcc2e30ce75ecb62006dc9f1eabd05a5e17cfeee
KrouriB <brice.krouri@gmail.com>
2023-09-13 14:26:46 +0200

    test repo

--- tests/Integration/ExampleTest.php
+++ tests/Integration/ExampleTest.php
@@ -1,5 +1,12 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+it('should have 10 item from the query where the ad is done', function(){
+    
+    $adRepository = $this->container->get(AdRepository::class);
+
+    $finished = $adRepository->findAll();
+    expect($finished)->toHaveCount(10);
 });
