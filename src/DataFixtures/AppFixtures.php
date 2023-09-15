bb14c43a135ed97580fd18e0a4eb6359652f0d29
KrouriB <brice.krouri@gmail.com>
2023-09-15 11:19:35 +0200

    different case of date

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,13 +11,39 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($count = 0; $count < 10; $count++) {
+        for ($count = 0; $count < 5; $count++) {
+            $ad = new Ad();
+
+            $ad->setWeight(3);
+            $ad->setImageSize(7964);
+            $ad->setStartedAt(new DateTimeImmutable('2023-07-30'));
+            $ad->setEndedAt(new DateTimeImmutable('2023-08-30'));
+            $ad->setUpdatedAt(new DateTimeImmutable('2023-09-13 09:14:24'));
+            $ad->setImage('montain-65017d70cf558466455447.png');
+
+            $manager->persist($ad);
+            $manager->flush();
+        }
+        for ($count = 0; $count < 5; $count++) {
             $ad = new Ad();
 
             $ad->setWeight(3);
             $ad->setImageSize(7964);
             $ad->setStartedAt(new DateTimeImmutable('2023-08-31'));
             $ad->setEndedAt(new DateTimeImmutable('2023-10-31'));
+            $ad->setUpdatedAt(new DateTimeImmutable('2023-09-13 09:14:24'));
+            $ad->setImage('montain-65017d70cf558466455447.png');
+
+            $manager->persist($ad);
+            $manager->flush();
+        }
+        for ($count = 0; $count < 5; $count++) {
+            $ad = new Ad();
+
+            $ad->setWeight(3);
+            $ad->setImageSize(7964);
+            $ad->setStartedAt(new DateTimeImmutable('2023-10-30'));
+            $ad->setEndedAt(new DateTimeImmutable('2023-11-30'));
             $ad->setUpdatedAt(new DateTimeImmutable('2023-09-13 09:14:24'));
             $ad->setImage('montain-65017d70cf558466455447.png');
 
