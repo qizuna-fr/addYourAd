b0c037832d380d2a7e652d295022604fabe1f8ba
KrouriB <brice.krouri@gmail.com>
2023-09-21 18:03:45 +0200

    acess direct image without access public storage

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -27,30 +27,39 @@ class AdController extends AbstractController
         return new JsonResponse(['url' => $ad->getImage() , 'lien' => $ad->getLink()], Response::HTTP_OK);
     }
 
+    #[Route('/link', name: 'image_link')]
+    public function imageLink()
+    {
+        return new JsonResponse(['ad' => $this->renderView('api/link.html.twig')], Response::HTTP_OK);
+    }
+
     #[Route('/image/{id}', name: 'app_image')]
     public function image(Request $request, AdRepository $adRepository, ImageBuilder $imageBuilder)
     {
         $ad = $adRepository->find($request->get('id'));
-        // dd($imageBuilder->makeImage($ad));
         return new Response
         (
             file_get_contents($imageBuilder->makeImage($ad)),
             Response::HTTP_OK,
             [
-                'Content-Disposition' => 'inline; filename="'.$ad->getImage().'"',
                 'Content-Type' => 'image/png'
             ]
         );
     }
 
-    // #[Route('/images', name: 'app_images')]
-    // public function images(AdRepository $adRepository): Response
-    // {
-    //     $ads = $adRepository->findAll();
-    //     return $this->render('ad/images.html.twig', [
-    //         'ads' => $ads,
-    //     ]);
-    // }
+    #[Route('/base64/{id}', name: 'app_base64')]
+    public function base64(Request $request, AdRepository $adRepository, ImageBuilder $imageBuilder)
+    {
+        $ad = $adRepository->find($request->get('id'));
+        return new Response
+        (
+            $imageBuilder->makeBase64ToImage($ad),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'image/png'
+            ]
+        );
+    }
 
     #[Route('/ad', name: 'app_ad')]
     public function index(AdRepository $adRepository): Response
