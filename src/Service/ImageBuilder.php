dc65d28f18877241dab7d9af4a08cbdf1083db80
KrouriB <brice.krouri@gmail.com>
2023-09-29 09:27:27 +0200

    type returned

--- src/Service/ImageBuilder.php
+++ src/Service/ImageBuilder.php
@@ -6,13 +6,13 @@ use App\Entity\Ad;
 
 class ImageBuilder
 {
-    public function makeImage(Ad $ad)
+    public function makeImage(Ad $ad): string
     {
         $path = 'img/uploads/';
         return $imagePath = $path . $ad->getImage();
     }
 
-    public function makeBase64ToImage(Ad $ad)
+    public function makeBase64ToImage(Ad $ad): string
     {
         return base64_decode($ad->getImageBase64());
     }
