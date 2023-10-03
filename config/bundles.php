4d9a65fe91d0f1249ad5de7c319c10544c7128f0
KrouriB <brice.krouri@gmail.com>
2023-10-03 15:05:00 +0200

    Symfony UX Chart.js

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,7 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
