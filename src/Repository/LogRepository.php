b1bee080ef79105e9bf758b851dd4059ad94e895
KrouriB <brice.krouri@gmail.com>
2023-10-03 12:48:10 +0200

    all raw sql request

--- src/Repository/LogRepository.php
+++ src/Repository/LogRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Log;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
@@ -24,26 +25,110 @@ class LogRepository extends ServiceEntityRepository
     }
 
     /**
-    * @return Log[] Returns an array of Log objects
-    */
+     * @return Log[] Returns an array of Log objects
+     */
     public function findAllLogs(): array
     {
-        $sql = "SELECT * FROM log";
+        $sql = "SELECT l.* FROM log l";
         $entityManager = $this->getEntityManager();
         $connection = $entityManager->getConnection();
         $request = $connection->prepare($sql);
-        $request->execute();
-        $result = $request->fetchAll();
-        return $result;
+        $result = $request->execute();
+        return $result->fetchAll();
     }
 
-//    public function findOneBySomeField($value): ?Log
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findClickLogs(): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.type = :type";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['type' => 'click']);
+        return $result->fetchAll();
+    }
+
+    /**
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findSeenLogs(): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.type = :type";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['type' => 'seen']);
+        return $result->fetchAll();
+    }
+
+    /**
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findDateLogs(DateTimeImmutable $minDate, DateTimeImmutable $maxDate): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.date >= :min AND l.date <= : max";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['min' => $minDate, 'max' => $maxDate]);
+        return $result->fetchAll();
+    }
+
+    /**
+     * @param int $id
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findByIdLogs(int $id): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.ad_id = :id";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['id' => $id]);
+        return $result->fetchAll();
+    }
+
+    /**
+     * @param int $id
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findByIdClickLogs(int $id): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.type = :type AND l.ad_id = :id";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['type' => 'click', 'id' => $id]);
+        return $result->fetchAll();
+    }
+
+    /**
+     * @param int $id
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findByIdSeenLogs(int $id): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.type = :type AND l.ad_id = :id";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['type' => 'seen', 'id' => $id]);
+        return $result->fetchAll();
+    }
+
+    /**
+     * @param int $id
+     * @return Log[] Returns an array of Log objects
+     */
+    public function findByIdDateLogs(DateTimeImmutable $minDate, DateTimeImmutable $maxDate, int $id): array
+    {
+        $sql = "SELECT l.* FROM log l WHERE l.date >= :min AND l.date <= : max AND l.ad_id = :id";
+        $entityManager = $this->getEntityManager();
+        $connection = $entityManager->getConnection();
+        $request = $connection->prepare($sql);
+        $result = $request->execute(['min' => $minDate, 'max' => $maxDate, 'id' => $id]);
+        return $result->fetchAll();
+    }
 }
