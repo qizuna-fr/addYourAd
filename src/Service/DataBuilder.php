0ce47071ccdf52cd960290fef72a6703ca17182a
KrouriB <brice.krouri@gmail.com>
2023-10-09 11:16:11 +0200

    methode to stack information per hours

--- src/Service/DataBuilder.php
+++ src/Service/DataBuilder.php
@@ -21,7 +21,6 @@ class DataBuilder
     public function dataPerHours(array $logs, DateTimeImmutable $date): array
     {
         $data = [];
-        $i = 0;
         $hours = (int) $date->format('H');
         $day = (int) $date->format('d');
         for($i = 0; $i < 25; $i++)
@@ -44,6 +43,46 @@ class DataBuilder
                 $hours = 0;
                 $day++;
             }
+        }
+        return $data;
+    }
+
+    /**
+     * @param Log[] $logs
+     */
+    public function allDataPerHours(array $logs): array
+    {
+        $data = [
+            0 => 0,
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+            6 => 0,
+            7 => 0,
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => 0,
+            12 => 0,
+            13 => 0,
+            14 => 0,
+            15 => 0,
+            16 => 0,
+            17 => 0,
+            18 => 0,
+            19 => 0,
+            20 => 0,
+            21 => 0,
+            22 => 0,
+            23 => 0
+        ];
+        foreach($logs as $log)
+        {
+            $logDate = new DateTimeImmutable($log["done_at"]);
+            $logHours = (int) $logDate->format('H');
+            $data[$logHours]++ ;
         }
         return $data;
     }
